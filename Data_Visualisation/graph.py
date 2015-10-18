ce7946026e28c795cc235e69842947a391eb6730
Arran <arrancardnell@gmail.com>
2015-10-18 13:26:11 +0100

    Wrapped graph functions in a class.

--- Data_Visualisation/graph.py
+++ Data_Visualisation/graph.py
@@ -4,41 +4,45 @@ import matplotlib.pyplot as plt
 from collections import Counter
 from parse import Parse
 
-def visualise_days():
-    """Visualise data by day of the week."""
-    Parser = Parse()
-    data_file = Parser.main()
-    # Returns a dict where it sums the total values for each key.
-    # In this case, the keys are the DaysOfWeek, and the values are
-    # a count of incidents.
-    counter = Counter(item["DayOfWeek"] for item in data_file)
 
-    # Separate out the counter to order it correctly when plotting.
-    data_list = [counter["Monday"],
-                 counter["Tuesday"],
-                 counter["Wednesday"],
-                 counter["Thursday"],
-                 counter["Friday"],
-                 counter["Saturday"],
-                 counter["Sunday"]
-                 ]
+class Graph:
 
-    day_tuple = tuple(["Mon", "Tues", "Wed", "Thurs", "Fri", "Sat", "Sun"])
+    def visualise_days(self):
+        """Visualise data by day of the week."""
+        data_parse = Parse()
+        data_file = data_parse.main()
+        # Returns a dict where it sums the total values for each key.
+        # In this case, the keys are the DaysOfWeek, and the values are
+        # a count of incidents.
+        counter = Counter(item["DayOfWeek"] for item in data_file)
 
-    # Assign the data to a plot
-    plt.plot(data_list)
+        # Separate out the counter to order it correctly when plotting.
+        data_list = [counter["Monday"],
+                     counter["Tuesday"],
+                     counter["Wednesday"],
+                     counter["Thursday"],
+                     counter["Friday"],
+                     counter["Saturday"],
+                     counter["Sunday"]
+                     ]
 
-    # Assign labels to the plot
-    plt.xticks(range(len(day_tuple)), day_tuple)
+        day_tuple = tuple(["Mon", "Tues", "Wed", "Thurs", "Fri", "Sat", "Sun"])
 
-    # Save the plot!
-    plt.savefig("Days.png")
+        # Assign the data to a plot
+        plt.plot(data_list)
 
-    # Close figure
-    plt.clf()
+        # Assign labels to the plot
+        plt.xticks(range(len(day_tuple)), day_tuple)
 
-def main():
-    visualise_days()
+        # Save the plot!
+        plt.savefig("Days.png")
+
+        # Close figure
+        plt.clf()
+
+    def main(self):
+        self.visualise_days()
 
 if __name__ == "__main__":
-    main()
+    Graph = Graph()
+    Graph.main()
